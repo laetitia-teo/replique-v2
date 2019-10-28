aa6ce3dfb51f1fac7b0ac864fcca72bc628dbe66
laetitia-teo <laetitia.teodorescu@gmail.com>
2019-10-28 22:29:54 +0100

    first commit

---
+++ README.md
@@ -0,0 +1,6 @@
+# Replique Project
+
+This a new version of the replique project.
+
+I preferred creating a new directory altogether, because the whole project is going to get rewritten. I need to change completely the scraper, maybe move beyond paintings as the main image source (we'll see for this, not yet decided). More fundamentally, we'll move the whole deep learning architectures to pytorch for simplicity.
+
---
+++ scraper/scraper.py
@@ -0,0 +1,26 @@
+"""
+File containing the implementation for the scaper. This piece of code is tasked
+with searching the web for images of naked people, of paintings of naked
+people.
+"""
+
+import scrapy
+
+# let's start by writing an example scraper, following the scrapy tutorial
+
+class QuotesSpider(scrapy.Spider):
+	name = 'quotes'
+	start_urls = [
+		'http://quotes.toscrape.com/tag/humor',
+	]
+
+	def parse(self, response):
+		for quote in response.css('div.quote'):
+			yield {
+				'text': quote.css('span.text::text').get(),
+				'author': quote.xpath('span/small/text()').get()
+			}
+
+		next_page = response.css('li.next a::attr("href")').get()
+		if next_page is not None:
+			yield response.follow(next_page, self.parse)
