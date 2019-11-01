4e8d61f0c9bb5606ed5ca6b5925e1639744a4eac
laetitia-teo <laetitia.teodorescu@gmail.com>
2019-11-01 13:44:48 +0100

    fixed settings

--- scraper/scraper/spiders/quote_spider.py
+++ scraper/scraper/spiders/quote_spider.py
@@ -5,19 +5,19 @@ Example spider, following the Scrapy tutorial.
 import scrapy
 
 class QuotesSpider(scrapy.Spider):
-	name = 'quotes'
+    name = 'quotes'
 
-	def start_requests(self):
-		urls = [
+    def start_requests(self):
+        urls = [
             'http://quotes.toscrape.com/page/1/',
             'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
-        	yield scrapy.Request(url=url, callback=self.parse)
+            yield scrapy.Request(url=url, callback=self.parse)
 
-	def parse(self, response):
-		page = response.url.split('/')[-2]
-		filename = 'quotes-%s.html' % page
-		with open(filename, 'wb') as f:
-			f.write(response.body)
-		self.log('Saved file %s' % filename)
+    def parse(self, response):
+        page = response.url.split('/')[-2]
+        filename = 'quotes-%s.html' % page
+        with open(filename, 'wb') as f:
+            f.write(response.body)
+        self.log('Saved file %s' % filename)
