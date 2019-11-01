98a16b89b7d676a4d535257fdc8b5915bb0b122d
laetitia-teo <laetitia.teodorescu@gmail.com>
2019-11-01 16:20:46 +0100

    finished scrapy basic tutorial

--- scraper/scraper/spiders/quote_spider.py
+++ scraper/scraper/spiders/quote_spider.py
@@ -10,14 +10,62 @@ class QuotesSpider(scrapy.Spider):
     def start_requests(self):
         urls = [
             'http://quotes.toscrape.com/page/1/',
-            'http://quotes.toscrape.com/page/2/',
         ]
         for url in urls:
             yield scrapy.Request(url=url, callback=self.parse)
 
     def parse(self, response):
-        page = response.url.split('/')[-2]
-        filename = 'quotes-%s.html' % page
-        with open(filename, 'wb') as f:
-            f.write(response.body)
-        self.log('Saved file %s' % filename)
+        # page = response.url.split('/')[-2]
+        # filename = 'quotes-%s.html' % page
+        # with open(filename, 'wb') as f:
+        #     f.write(response.body)
+        # self.log('Saved file %s' % filename)
+        for quote in response.css('div.quote'):
+            yield {
+                'text': quote.css('span.text::text').get(),
+                'author': quote.css('small.author::text').get(),
+                'tags': quote.css('div.tags a.tag::text').getall()
+            }
+
+        # next_page = response.css('li.next a::attr(href)').get()
+        # if next_page is not None:
+        #     next_page = response.urljoin(next_page)
+        #     yield scrapy.Request(next_page, callback=self.parse)
+        # # alternative version, with follow methot that returns a Request object
+        # next_page = response.css('li.next a::attr(href)').get()
+        # if next_page is not None:
+        #     yield response.follow(next_page, callback=self.parse)
+        # # variant for following links : passing selectors :
+        # for href in response.css('li.next a::attr(href)'):
+        #     yield response.follow(href, callback=self.parse)
+        # simpler variant, shortcut for <a> elements
+        for a in response.css('li.next a'):
+            yield response.follow(a, callback=self.parse)
+
+class AuthorSpider(scrapy.Spider):
+    """
+    Spider for retrieving author information.
+    """
+    name = 'author'
+
+    # alternative way of initializing urls
+    start_urls = ['http://quotes.toscrape.com/']
+
+    def parse(self, response):
+        # follow links to author pages
+        for href in response.css('.author + a::attr(href)'):
+            yield response.follow(href, self.parse_author)
+
+        # follow pagination links
+        for href in response.css('li.next a::attr(href)'):
+            yield response.follow(href, self.parse)
+
+    def parse_author(self, response):
+        def extract_with_css(query):
+            return response.css(query).get(default='').strip()
+
+        yield {
+            'name': extract_with_css('h3.author-title::text'),
+            'birthdate': extract_with_css('.author-born-date::text'),
+            'bio': extract_with_css('.author-description::text'),
+        }
